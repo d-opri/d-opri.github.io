d14e22e8720b54ba216a3ef5f5520c8751c88dda
d-opri <danieo.ux@gmail.com>
2023-04-27 13:27:19 +0200

    add footer, add layout component

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  text-align: center;
+  margin: auto;
+`;
+
+export default function Footer() {
+  return (
+    <StyledFooter>
+      <p>@DaniOpri</p>
+    </StyledFooter>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-
-import Navbar from "../../components/Navbar";
+import Layout from "../../components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <Navbar />;
+  return <Layout />;
 }
