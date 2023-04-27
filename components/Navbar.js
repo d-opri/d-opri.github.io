e080410da40a0376e3aa4761c863f85778a250dc
d-opri <danieo.ux@gmail.com>
2023-04-27 12:47:01 +0200

    add links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,40 @@
-import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  position: sticky;
+  margin: auto;
+`;
+
+const StyledList = styled.ul`
+  display: flex;
+  alignt-items: center;
+  gap: 2rem;
+  list-style: none;
+`;
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <StyledNav>
+      <Link href="/">
+        <h1>Dev</h1>
+      </Link>
+
+      <StyledList>
+        <li>
+          <Link href="/">Work</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </StyledList>
+    </StyledNav>
+  );
 };
 
 export default Navbar;
