d607b04d08b648cddb708ed6e788dfd8c3587f46
d-opri <danieo.ux@gmail.com>
2023-04-28 15:07:12 +0200

    add links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,20 +3,25 @@ import styled from "styled-components";
 
 const StyledSection = styled.section`
   padding: 100px 20px;
+  display: flex;
+  align-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
+  padding: 6.25rem 1.25rem;
 `;
 const StyledLink = styled(Link)`
   color: red;
 `;
 
-const contact = () => {
+const Contact = () => {
   return (
     <StyledSection>
-      <StyledLink href="/contact">Github</StyledLink>
-      <StyledLink href="/contact">Behance</StyledLink>
-      <StyledLink href="/contact">Xing</StyledLink>
-      <StyledLink href="/contact">Email</StyledLink>
+      <StyledLink href="/">dani.opri@protonmail.com </StyledLink>
+      <StyledLink href="/">Github</StyledLink>
+      <StyledLink href="/">Behance</StyledLink>
+      <StyledLink href="/">Xing</StyledLink>
     </StyledSection>
   );
 };
 
-export default contact;
+export default Contact;
