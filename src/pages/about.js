80c389946d80ced39aeb87744c1aa9f378ee0411
d-opri <danieo.ux@gmail.com>
2023-04-28 15:11:53 +0200

    add about section

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  padding: 3rem;
+`;
+
+export default function About() {
+  return (
+    <StyledSection>
+      <h1>About</h1>
+      <p>yes yes you have to include this bitch</p>
+    </StyledSection>
+  );
+}
