c688211e681cf39a09ad904d0be1989f56464747
d-opri <danieo.ux@gmail.com>
2023-04-29 12:31:24 +0200

    add about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,51 @@
 import styled from "styled-components";
 
-const StyledSection = styled.section`
-  padding: 3rem;
+const Container = styled.main`
+  position: relative;
+  margin-left: 15%;
+`;
+
+const Title = styled.h1`
+  font-weight: 500;
+`;
+
+const Section = styled.section`
+  margin-right: 10%;
+  transform: translateY(30%);
+`;
+const Content = styled.article`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  text-align: left;
 `;
 
 export default function About() {
   return (
-    <StyledSection>
-      <h1>About</h1>
-      <p>yes yes you have to include this bitch</p>
-    </StyledSection>
+    <>
+      <Container>
+        <Section>
+          <Title>Moin Moin</Title>
+          <Content>Here talking about self bla bla bla</Content>
+        </Section>
+        <Section>
+          <Title>Skills</Title>
+          <Content>
+            <br />
+            React
+            <br />
+            Node JS
+            <br />
+            Design system
+            <br />
+            Wireframing & Prototyping
+            <br />
+            Figma
+            <br />
+          </Content>
+        </Section>
+      </Container>
+    </>
   );
 }
