5eda6056dca2fa8ff518bd54061d8a8e538fc6c5
d-opri <danieo.ux@gmail.com>
2023-06-21 17:22:38 +0200

    fix fetch url

--- components/sections/Work.js
+++ components/sections/Work.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { useFetch } from "../../utils/useFetch";
 
 export default function Work() {
-  const { data, isLoading, isError } = useFetch("json/projects.json");
+  const { data, isLoading, isError } = useFetch("/json/projects.json");
 
   if (isLoading) {
     return <div>Loading...</div>;
